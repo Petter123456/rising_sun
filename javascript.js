3fe4b77943f418c201c2b7ab6e790b3329afa5f8
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-18 11:47:44 -0500

    finished to 3f of readme

--- javascript.js
+++ javascript.js
@@ -4,12 +4,20 @@ window.addEventListener('load', function(){
 });
 
 function getWeatherData() {
-  console.log(this.responseText); // Check your dev tools in the browser and you can see the Hash.
+  const data = (JSON.parse(this.responseText)); //Parsing the response into a JSON format.
+
+//const data = (this.responseText); //just getting the hash response, check your dev.tools.
+
+  console.dir(data); // Check your dev tools in the browser and you can see the object.
+  const sunrise = data.sys.sunrise //entering the sys key and the the sunrise key
+  const sunset = data.sys.sunset
 }
 
 const connection_open_weather_map = new XMLHttpRequest(); // make XML request
 
-connection_open_weather_map.addEventListener("load", getWeatherData); //on load run getWeatherData function
+connection_open_weather_map.addEventListener("load", getWeatherData); //make an xml request and on load run getWeatherData function
 
-connection_open_weather_map.open("GET", weather_api);
-connection_open_weather_map.send();
+connection_open_weather_map.open("GET", weather_api); //open an xmlrequest that is GET the const which specified the api
+connection_open_weather_map.send(); //The XMLHttpRequest.send() method sends the request.
+
+connection_open_weather_map
